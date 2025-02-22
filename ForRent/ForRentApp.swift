38032a3a693c265bc95466b14cebf7fba629e475
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-02-21 21:28:32 -0500

    Initial Commit

---
+++ ForRent/ForRentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForRentApp.swift
+//  ForRent
+//
+//  Created by Eddie Nguyen on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct ForRentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForRentTests/ForRentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForRentTests.swift
+//  ForRentTests
+//
+//  Created by Eddie Nguyen on 2025-02-21.
+//
+
+import Testing
+@testable import ForRent
+
+struct ForRentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
